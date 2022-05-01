b2c45478946888d6056508077234b86113aec2ac
devmaxwel <eng.maxwel.ochieng@gmail.com>
2022-05-01 10:56:48 +0300

    Teleporting, Slots,custom events,event modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Hello There</p>
+  <div v-if="showModal">
+    <Modal theme="sale">
+
+      <!-- Dynamic slot / It must have a name that we will use to call it in the main component are  -->
+      <template v-slot:links>
+        <a href="#">The Maxwel</a>
+        <a href="#">The Ochieng</a>
+    </template>
+      <div>
+        <h1>Hello Maxwel</h1>
+        <p>Grab your deal from KFC now</p>
+      </div>
+    </Modal>
+  </div>
+  <span @click="handleShow">
+    <button v-if="!showModal">Show Modal</button>
+    <br /><br />
+    <button v-show="showModal">Hide Modal</button>
+  </span>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "../src/components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      header: "Hello Maxwel",
+      text: " Grab your deal from KFC now",
+      showModal: false,
+    };
+  },
+  methods: {
+    handleShow() {
+      this.showModal = !this.showModal;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="{ modal: theme === 'sale' }">
+    <slot />
+    <p>The Modal</p>
+    <div class="actions">
+        <!-- When we want a dynamic slot   -->
+      <slot name="links" />
+    </div>
+  </div>
+</template>
+
+// props 
+
+<script>
+export default {
+  name: "Modal",
+  props: ["theme"],
+  data() {},
+};
+</script>
+
+<style scoped>
+h1 {
+  color: pink;
+}
+
+.modal {
+  background: crimson;
+  width: 300px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  color: #fff;
+}
+</style>
